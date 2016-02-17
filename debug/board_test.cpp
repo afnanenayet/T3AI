1f6bb27f6241ddcaef2438855584f87829c441cd
Afnan Enayet <afnan.enayet@gmail.com>
2016-02-16 20:59:54 -0500

    added memory check files

---
+++ debug/board_test.cpp
@@ -0,0 +1,5 @@
+#include "board.hpp"
+
+int main(){
+    board test_board;
+}
---
+++ debug/valgrind_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+g++ -std=gnu++14 ../*.cpp -o T3AI_debug;
+valgrind --leak-check=yes ../T3AI_debug > mem_check_results.txt;
